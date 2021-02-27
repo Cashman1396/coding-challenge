eab216d1390d354d7a4f9e8a5b5fd2937243df24
Cashman1396 <Chrisashman96@gmail.com>
2021-02-27 12:11:53 -0500

    worked on header

--- src/components/Pets.jsx
+++ src/components/Pets.jsx
@@ -1,3 +1,26 @@
 import React, { useState } from 'react'
-import PetController from './Pets/PetController'
+import PetController from './Pet/PetController'
+
+import './style.css'
+
+function Pets(props) {
+    const { pets } = props
+
+    const [list, stateList] = useState([])
+    
+    return (
+        <main>
+            { pets.map((pet, index) => (
+                <div className="container">
+                    <PetController key={index}, pet={pet} list={list}, stateList={stateList}/>
+                </div>
+            ))}
+            {list.length > 0 ? }
+        </main>
+    )
+}
+
+
+export default Pets 
+
 
---
+++ src/components/header/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <h1 className="Header">Welcome to the Pet Gallery</h1>
+            <br/>
+            <h5>If you see Picture you like do not hesitant to click DOWNLOAD</h5>
+        </header>
+    )
+}
+
+export default Header
